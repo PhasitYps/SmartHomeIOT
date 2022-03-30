a517766c8a1b57698898da8b6f84c0400ff8fb34
63OO36O PHASIT YAPRASAT <phasit.y63@rsu.ac.th>
2022-03-30 19:58:40 +0700

    -init

--- app/src/main/java/com/example/smarthomeiot/MapActivity.kt
+++ app/src/main/java/com/example/smarthomeiot/MapActivity.kt
@@ -1,7 +1,9 @@
 package com.example.smarthomeiot
 
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smarthomeiot.master.GPSManage
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -19,17 +21,37 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback{
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        //13.668217, 100.614021
+
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(
-            MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val thailand = LatLng(13.668217, 100.614021)
+        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(thailand, 16f))
+
+        initMap()
+    }
+
+    private fun initMap(){
+
+        val gpsManage = GPSManage(this)
+        gpsManage.requestGPS()
+        gpsManage.setMyEvent(object : GPSManage.MyEvent{
+            override fun onLocationChanged(currentLocation: Location) {
+
+                val current = LatLng(currentLocation.latitude, currentLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 16f))
+            }
+
+            override fun onDissAccessgps() {
+
+            }
+        })
     }
 }
